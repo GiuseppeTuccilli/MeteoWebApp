aeb28f92e1ac2e29e4caa32a6fc6e265f3a25392
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2026-05-04 21:40:40 +0200

    inst-sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./style/custom-bootstrap.scss";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 import MyNavbar from "./components/MyNavbar.jsx";
