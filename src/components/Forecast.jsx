526ed564ed8e2ddacc30aa99ba1be093fe09ccef
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2026-05-06 20:41:23 +0200

    fix

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getForecast, stringToDate } from "../files/functions";
 import Nav from "react-bootstrap/Nav";
+import { Spinner } from "react-bootstrap";
 
 const Forecast = function () {
   const params = useParams();
@@ -53,86 +54,95 @@ const Forecast = function () {
 
   return (
     <>
-      <div className="d-flex  text-white align-items-center  ">
-        <div className="p-2 border border-1 border-secondary-subtle bg-secondary h4rem d-flex align-items-center flex-grow-1 justify-content-center">
-          <h2 className="m-0">
-            Città:{" "}
-            <span className="text-decoration-underline text-info">
-              {forecast.city.name}
-            </span>
-          </h2>
+      {" "}
+      {loading ? (
+        <div className="d-flex justify-content-center mt-3">
+          <Spinner />
         </div>
-        <div className="p-2 border-border-1 border-secondary bg-primary-subtle text-dark h4rem d-flex align-items-center">
-          <h3 className="m-0">
-            Paese:{" "}
-            <span className="text-decoration-underline">
-              {forecast.city.country}
-            </span>
-          </h3>
-        </div>
-      </div>
-      <Nav
-        justify
-        variant="tabs"
-        defaultActiveKey="0"
-        className="d-none d-md-flex"
-      >
-        <Nav.Item>
-          <Nav.Link
-            eventKey="0"
-            onClick={() => {
-              setActive("0");
-            }}
-            className="fw-bold"
+      ) : (
+        <>
+          <div className="d-flex  text-white align-items-center  ">
+            <div className="p-2 border border-1 border-secondary-subtle bg-secondary h4rem d-flex align-items-center flex-grow-1 justify-content-center">
+              <h2 className="m-0">
+                Città:{" "}
+                <span className="text-decoration-underline text-info">
+                  {forecast.city.name}
+                </span>
+              </h2>
+            </div>
+            <div className="p-2 border-border-1 border-secondary bg-primary-subtle text-dark h4rem d-flex align-items-center">
+              <h3 className="m-0">
+                Paese:{" "}
+                <span className="text-decoration-underline">
+                  {forecast.city.country}
+                </span>
+              </h3>
+            </div>
+          </div>
+          <Nav
+            justify
+            variant="tabs"
+            defaultActiveKey="0"
+            className="d-none d-md-flex"
           >
-            Oggi
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link
-            eventKey="1"
-            onClick={() => {
-              setActive("1");
-            }}
-            className="fw-bold"
-          >
-            {secondDay.toString().slice(4, 10)}
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link
-            eventKey="2"
-            onClick={() => {
-              setActive("2");
-            }}
-            className="fw-bold"
-          >
-            {thirdDay.toString().slice(4, 10)}
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link
-            eventKey="3"
-            onClick={() => {
-              setActive("3");
-            }}
-            className="fw-bold"
-          >
-            {fourthDay.toString().slice(4, 10)}
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link
-            eventKey="4"
-            onClick={() => {
-              setActive("4");
-            }}
-            className="fw-bold"
-          >
-            {fifthDay.toString().slice(4, 10)}
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
+            <Nav.Item>
+              <Nav.Link
+                eventKey="0"
+                onClick={() => {
+                  setActive("0");
+                }}
+                className="fw-bold"
+              >
+                Oggi
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                eventKey="1"
+                onClick={() => {
+                  setActive("1");
+                }}
+                className="fw-bold"
+              >
+                {secondDay.toString().slice(4, 10)}
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                eventKey="2"
+                onClick={() => {
+                  setActive("2");
+                }}
+                className="fw-bold"
+              >
+                {thirdDay.toString().slice(4, 10)}
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                eventKey="3"
+                onClick={() => {
+                  setActive("3");
+                }}
+                className="fw-bold"
+              >
+                {fourthDay.toString().slice(4, 10)}
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                eventKey="4"
+                onClick={() => {
+                  setActive("4");
+                }}
+                className="fw-bold"
+              >
+                {fifthDay.toString().slice(4, 10)}
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </>
+      )}
     </>
   );
 };
