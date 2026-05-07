3b80a52b19f34608b7f008609d0b407b432f125c
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2026-05-07 23:18:10 +0200

    fix

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -2,10 +2,16 @@ const MyFooter = function () {
   return (
     <>
       <div className="myFooter d-flex align-items-center justify-content-evenly">
-        <a href="#">
+        <a
+          href="https://www.linkedin.com/in/giuseppe-tuccilli/"
+          target="_blank"
+        >
           <i className="bi bi-linkedin"></i> Linkedin
         </a>
-        <a href="#">
+        <a
+          href="https://giuseppetuccilli-developer-portfolio.netlify.app"
+          target="_blank"
+        >
           <i className="bi bi-journal"></i> Portfolio
         </a>
       </div>
